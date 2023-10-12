4e65f3cac525aac49ed8035e23628d099a4a2ba9
Sebastian Pape <s@pape.io>
2023-10-12 10:53:50 +0200

    express example

---
+++ README.md
@@ -0,0 +1,18 @@
+## Prerequisites
+
+1. Create managed integration on https://app.depay.com
+
+2. Set `Callback` for your integration on app.depay.com e.g. `https://yourdomain.com/depay/callback`
+
+
+### Dynamic Configurations (optional)
+
+Optionally you can configure "dynamic configurations", in order to provide a payment configuration dynamically depending on "payload" parameters.
+
+3. Enable "Dynamic Configuration" for your integration on app.depay.com
+
+4. Set `Endpoint` for your integration on app.depay.com e.g. `https://yourdomain.com/depay/configuration`
+
+5. Create a private & public key as documented here: https://depay.com/docs/payments/integrate/widget#create-privatepublic-key
+
+6. Enter the `Public Key` for your integration on app.depay.com
---
+++ src/server.ts
@@ -0,0 +1,134 @@
+const { Buffer } = require("node:buffer");
+import bodyParser from 'body-parser';
+import crypto from 'node:crypto';
+import express, {Express, Request, Response} from 'express';
+import { verify } from '@depay/js-verify-signature';
+
+const app: Express = express();
+const port = 3000;
+app.use(bodyParser.json());
+
+//
+// THE FOLLOWING PLACEHOLDERS NEED TO BE CONFIGURED!
+
+// Identifies your integration
+const integrationId:string = "SET_YOUR_INTEGRATION_ID";
+
+// Used to verify communication from DePay APIs to your integration
+// Provided by app.depay.com
+const publicKey:string = "-----BEGIN PUBLIC KEY----- PLACE PUBLIC KEY HERE!!! -----END PUBLIC KEY-----";
+
+// Used to sign & authenticate communication from your integration to DePay APIs
+// Create and provide as documented here: https://depay.com/docs/payments/integrate/widget#create-privatepublic-key
+const privateKey:any = process.env.MY_PRIVATE_KEY ? crypto.createPrivateKey(process.env.MY_PRIVATE_KEY) : undefined;
+
+app.get('/', (req: Request, res: Response)=>{
+
+  const payload = {
+    itemId: 1,
+    quantity: 2,
+    userId: '123'
+  };
+
+  res.send(`\
+  <html>\
+    <head>\
+      <script defer async src="https://integrate.depay.com/widgets/v12.js"></script>\
+    <head>\
+    </head>\
+    <body style="text-align: center; padding-top: 5rem;">\
+      <button style="font-size: 3rem;" onClick="DePayWidgets.Payment({ integration: '${integrationId}', payload: '${JSON.stringify(payload).replace(/"/g, '&quot;')}' });">\
+        Pay Now\
+      </button>\
+    </body>\
+  </html>\
+  `);
+});
+
+const verifyRequest = async(req: Request): Promise<boolean>=>{
+  
+  return await verify({
+    signature: req.headers['x-signature'],
+    data: req.body,
+    publicKey,
+  });
+}
+
+const getResponseSignature = (responseString: string): string=>{
+
+  const signature = crypto.sign('sha256', Buffer.from(responseString), {
+    key: privateKey,
+    padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+    saltLength: 64,
+  });
+
+  const urlSafeBase64Signature = signature.toString('base64')
+    .replace('+', '-')
+    .replace('/', '_')
+    .replace(/=+$/, '');
+
+  return urlSafeBase64Signature;
+}
+
+app.post('/depay/configuration', async(req: Request, res: Response)=>{
+
+  if(!await verifyRequest(req)){
+    return res.status(401).json({ error: "UNAUTHORIZED" });
+  };
+
+  const price = 1.00;
+
+  const configuration = {
+    accept: [
+      { blockchain: 'ethereum', token: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', amount: price*req.body.quantity }
+    ]
+  };
+
+  // If you need to dynamically set a redirect location after a successfull payment, set forward_to
+  //
+  // configuration.forward_to = "https://mydomain.com/payment/confirmation/SOMEID"
+  //
+
+  res.setHeader('x-signature', getResponseSignature(JSON.stringify(configuration)));
+  res.status(200).json(configuration);
+});
+
+app.post('/depay/callback', async(req: Request, res: Response)=>{
+
+  if(!await verifyRequest(req)){
+    return res.status(401).json({ error: "UNAUTHORIZED" });
+  };
+
+  // Do whatever you need to do after a succesfull payment
+  //
+  // req.body:
+  //
+  // {
+  //   "blockchain": "polygon",
+  //   "transaction": "0x053279fcb2f52fd66a9367416910c0bf88ae848dca769231098c4d9e240fcf56",
+  //   "sender": "0x317D875cA3B9f8d14f960486C0d1D1913be74e90",
+  //   "receiver": "0x08B277154218CCF3380CAE48d630DA13462E3950",
+  //   "token": "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+  //   "amount": "0.0985",
+  //   "payload": null,
+  //   "after_block": "46934392",
+  //   "commitment": "confirmed",
+  //   "confirmations": 1,
+  //   "created_at": "2023-08-30T11:37:30.157555Z",
+  //   "confirmed_at": "2023-08-30T11:37:35.492041Z"
+  // }
+
+  const responseData = {};
+
+  // If you need to dynamically set a redirect location:
+  //
+  // responseData.forward_to = "https://mydomain.com/payment/confirmation/SOMEID"
+  //
+
+  res.setHeader('x-signature', getResponseSignature(JSON.stringify(responseData)));
+  res.status(200).json(responseData);
+});
+
+app.listen(port, ()=> {
+  console.log(`[Server]: I am running at https://localhost:${port}`);
+});
